d60c70b8ba8be8da1ef8a6bedbaad9b8b7c6b3fa
MasatoKato <152849350+katopery@users.noreply.github.co>
2024-01-20 15:39:00 +0000

    [Add] Top,Aboutページ作成

--- db/migrate/20240119161327_create_books.rb
+++ db/migrate/20240119161327_create_books.rb
@@ -2,8 +2,9 @@ class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
 
-      t.string  :title  #本のタイトル
-      t.text    :body   #感想
+      t.string  :title    #本のタイトル
+      t.text    :body     #感想
+      t.integer :user_id  #外部キー
       t.timestamps
     end
   end
